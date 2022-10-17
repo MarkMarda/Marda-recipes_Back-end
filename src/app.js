1744e32f18c9ba7fbade5c91045165cd6238af97
Marda <marda_da-capo@hotmail.com>
2022-10-17 11:14:09 -0500

    general config

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const config = require("./config");
+
+app.use(express.json());
+
+app.listen(config.port, () => {
+
+  console.log(`Server started at port ${config.port}`);
+
+});
---
+++ src/utils/crypto.js
@@ -0,0 +1,20 @@
+const bcrypt = require("bcrypt");
+
+//encrypts the password created
+const hashPassword = (plainPassword) => {
+
+  return bcrypt.hashSync(plainPassword, 10);
+
+};
+
+//To compare the password received with the pass of db
+const comparePassword = (plainPassword, hashedPassword) => {
+
+  return bcrypt.compareSync(plainPassword, hashedPassword);
+
+};
+
+module.exports = {
+  hashPassword,
+  comparePassword
+};
